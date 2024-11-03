e16afa79b80602cbea45692ab7d49c7b66cce097
Masterokadanori <127539685+Masterokadanori@users.noreply.github.com>
2024-11-03 23:11:58 +0700

    Add files via upload

---
+++ js/register.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAPK1U8CDFlxyoMGFluVzEPM0bZ5ZTkAvk",
+    authDomain: "chuyenanh-63ad4.firebaseapp.com",
+    projectId: "chuyenanh-63ad4",
+    storageBucket: "chuyenanh-63ad4.firebasestorage.app",
+    messagingSenderId: "275605951340",
+    appId: "1:275605951340:web:ead0f7a34f455b1bc7d949"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
